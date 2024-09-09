a5208c004a9a3abc33748034fc62a37b08fd7ff2
Safonov Anton Vyacheslavovich <SafonovAV@cloudx.group>
2024-09-09 12:34:28 +0300

    Add files to repo

---
+++ lib/typings.d.ts
@@ -0,0 +1,31 @@
+
+export = VersionCheckerPlugin;
+
+declare class VersionCheckerPlugin {
+    constructor(options?: VersionCheckerPlugin.Options);
+
+    apply(compiler: Compiler): void;
+
+    private messages: string[];
+    private depNames: VersionCheckerPlugin.Options.depNames;
+    private options: VersionCheckerPlugin.Options.options;
+
+    static checkVersion(
+        name: string
+    ): Promise<void>;
+
+    static showToast(): void;
+    static showConsole(): void;
+}
+
+declare namespace VersionCheckerPlugin {
+    interface Options {
+        depNames: string[];
+        options: {
+            showToast: boolean;
+            showConsole: boolean;
+            devServerOnly: boolean;
+            disableCertValid: boolean;
+        };
+    }
+}
